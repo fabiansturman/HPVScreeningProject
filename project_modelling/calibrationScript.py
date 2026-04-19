082f41c8e821aeae2c278783f1cc8ba247667bf8
Fabian Sturman <fabiansturman@hotmail.co.uk>
2026-04-19 19:43:22 +0100

    preparing new cal

--- project_modelling/calibrationScript.py
+++ project_modelling/calibrationScript.py
@@ -78,11 +78,13 @@ if __name__=="__main__":
 
     -> Apr16_1__65_68_76_55_55_9_9__96_7_13 : COMPLETE this is a cal (on BMRC) doing a Sensitivity Analysis on cancer treatment effectiveness (trying 65% rather than 80%) (otherwise, it is identical to the setup in Apr14_1_USINGNATSALANDLIT__8_68_76_55_55_9_9__96_7_13)
     -> Apr17_1__65_68_76_55_55_9_9__96_7_13 : COMPLETE this is a cal (on MB) the same as above
-    -> Apr17_2__65_68_76_55_55_9_9__96_7_13 : INPROGRESS this is a cal (on BMRC) the same as above
+    -> Apr17_2__65_68_76_55_55_9_9__96_7_13 : DONE BUT NOT YET TRANSFERRED this is a cal (on BMRC) the same as above
 
-    -> Apr18_1__8_68_76_36_36_8_8__96_7_13 : INPROGRESS this is a cal (on MB) doing a Sensitivity Analysis doing a lower bound on Sensitivity Analysis on Screening Algorothm Compliance (otherwise is identical to the setup in Apr14_1_USINGNATSALANDLIT__8_68_76_55_55_9_9__96_7_13)
-    -> Apr18_2__8_68_76_36_36_8_8__96_7_13 : INPROGRESS this is a cal (on XPS) as above
+    -> Apr18_1__8_68_76_36_36_8_8__96_7_13 : DONE this is a cal (on MB) doing a Sensitivity Analysis that is a lower bound on Screening Algorothm Compliance (otherwise is identical to the setup in Apr14_1_USINGNATSALANDLIT__8_68_76_55_55_9_9__96_7_13)
+    -> Apr18_2__8_68_76_36_36_8_8__96_7_13 : INPROGRESS this is a cal (on XPS - though if the XPS crashed again before 4000 trials idk maybe i will stop trying to run these cals on XPS and just have 2 cals at any one moment running) as above
     -> Apr18_4__8_68_76_36_36_8_8__96_7_13 : INPROGRESS this is a cal (on BMRC) as above
+
+    -> Apr19_1__8_8_8_68_68_95_95__96_7_13 : INPROGRESS this is a cal (on MB) doing a Sensitivity Analysis that is an a upper bound on Screening Algorothm Compliance (otherwise is identical to the setup in Apr14_1_USINGNATSALANDLIT__8_68_76_55_55_9_9__96_7_13)
 
         
     ^^^Once the above 6 cals are done I can then be informed about the pair of mixing matrices I select, make the final cal according to the three (or more if i feel like 5 do 5) of the mixing matrix pair i select, and move on! (I really would prefer the latter ones as I can justify where they came from, and they fit a more expected/less noisy shape/'more parsimonious in that it is more simple' shape)^^^
@@ -90,7 +92,7 @@ if __name__=="__main__":
     """
 
     # Create the calibration object, run it, and plot the results
-    name =  f"project_modelling/calibration_results/Apr18_4_{calibration_code}" # "calibration_results/Feb14_1" #"calibration_results/Feb13_1"
+    name =  f"project_modelling/calibration_results/Apr19_1_{calibration_code}" # "calibration_results/Feb14_1" #"calibration_results/Feb13_1"
     
     calib = hpv.Calibration(
         sim,
@@ -99,7 +101,7 @@ if __name__=="__main__":
         datafiles=datafiles,
 
         total_trials=5000,
-        n_workers=8, #to be changed according to the multiprocessing capability of the hardware being used
+        n_workers=5, #to be changed according to the multiprocessing capability of the hardware being used
 
         keep_db=True,
         name=name
