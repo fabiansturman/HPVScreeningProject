c1983223e32ecdad1b8c92332b3711ece17f6314
Fabian Sturman <fabiansturman@hotmail.co.uk>
2026-04-22 07:37:32 +0100

    updating naming of algs in scenario_configuration

--- project_modelling/scenario_configuration.py
+++ project_modelling/scenario_configuration.py
@@ -8,7 +8,7 @@ from InterventionAlgorithms import screeningAlgorithms, NHS_Vacc, GlobalScreenin
 
 
 ##THE FOLLOWING MAY BE CHANGED TO SET DIFFERENT INTERVENTIONS##
-screening_algorithm_stub_name = '5_5' #set the name we will be using for screening algorithms
+screening_algorithm_stub_name = 'V5U5A5' #set the name we will be using for screening algorithms
 screening_algorithm = screeningAlgorithms.get_interventions(v=5,u=5,a=5)
 """                                                         ^
                                                             v = screening interval for vaccinated individuals
