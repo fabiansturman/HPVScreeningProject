193cadf31ea19c21b6aad1cb7754634a27088b14
Fabian Sturman <fabiansturman@hotmail.co.uk>
2026-04-25 20:49:29 +0100

    Update calibrationScript.py

--- project_modelling/calibrationScript.py
+++ project_modelling/calibrationScript.py
@@ -92,7 +92,7 @@ if __name__=="__main__":
         => ** **
 
     -> Apr22_3__8_68_76_55_55_9_9_1_96_7_13 : COMPLETE this is a cal (on BMRC, copied to MB) doing a Sensitivity Analysis considering ohr rel imm of 0.1. (otherwise is identical to the setup in Apr14_1_USINGNATSALANDLIT__8_68_76_55_55_9_9__96_7_13)
-    -> Apr23_1__8_68_76_55_55_9_9_1_96_7_13 : INPROGRESS this is a cal (on XPS) as above     
+    -> Apr23_1__8_68_76_55_55_9_9_1_96_7_13 : COMPLETE this is a cal (on XPS, copied to MB) as above     
     -> Apr23_3__8_68_76_55_55_9_9_1_96_7_13 : COMPLETE this is a cal (on MB) as above
 
     -> Apr21_1__8_68_76_55_55_9_9__96_7_62 : COMPLETE this is a cal (on XPS, copied to MB) doing a Sensitivity Analysis on Initial HPV Prevalence among Men, seeing what will happen if we make it drop off with age like we do for women (otherwise is identical to the setup in Apr14_1_USINGNATSALANDLIT__8_68_76_55_55_9_9__96_7_13)
